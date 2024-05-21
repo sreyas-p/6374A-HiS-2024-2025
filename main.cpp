92225f6d1f40f5827b87c712e2968973633803fd
sreyas-p <150875815+sreyas-p@users.noreply.github.com>
2024-05-20 22:08:39 -0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,176 @@
+#include "main.h"
+#include "lemlib/api.hpp"
+#include "lemlib/chassis/trackingWheel.hpp"
+
+pros::Controller controller(pros::E_CONTROLLER_MASTER);
+
+pros::Motor left_front_motor(1, pros::E_MOTOR_GEARSET_06, false); // port 1, blue gearbox, not reversed
+pros::Motor right_front_motor(2, pros::E_MOTOR_GEARSET_06, false); 
+pros::Motor right_middle_motor(3, pros::E_MOTOR_GEARSET_06, false); 
+pros::Motor left_middle_motor(4, pros::E_MOTOR_GEARSET_06, false); 
+pros::Motor right_back_motor(5, pros::E_MOTOR_GEARSET_06, false); 
+pros::Motor left_back_motor(6, pros::E_MOTOR_GEARSET_06, false); 
+
+pros::MotorGroup left_side_motors({left_front_motor, left_back_motor, left_middle_motor});
+pros::MotorGroup right_side_motors({right_front_motor, right_back_motor, right_middle_motor});
+
+pros::Imu imu(7);
+
+// left tracking wheel encoder
+pros::Rotation back_rot(8, false); 
+pros::Rotation front_rot(9, false); 
+
+lemlib::TrackingWheel front_tracking_wheel(&back_rot, 2.75, 4.3, 1);
+lemlib::TrackingWheel back_tracking_wheel(&front_rot, 2.75, 4.3, 1);
+
+lemlib::OdomSensors_t sensors {
+    &front_tracking_wheel, // vertical tracking wheel
+	nullptr,
+    &back_tracking_wheel, // horizontal tracking wheel 
+	nullptr,
+    // we don't have a second tracking wheel, so we set it to nullptr
+    &imu // inertial sensor
+};
+
+lemlib::Drivetrain_t drivetrain {
+    &left_side_motors, // left drivetrain motors
+    &right_side_motors, // right drivetrain motors
+    10, // track width
+    3.25, // wheel diameter
+    450 // wheel rpm
+};
+
+// lateral motion controller
+lemlib::ChassisController_t lateralController {
+    8, // kP
+    30, // kD
+    1, // smallErrorRange
+    100, // smallErrorTimeout
+    3, // largeErrorRange
+    500, // largeErrorTimeout
+    5 // slew rate
+};
+ 
+// turning PID
+lemlib::ChassisController_t angularController {
+    4, // kP
+    40, // kD
+    1, // smallErrorRange
+    100, // smallErrorTimeout
+    3, // largeErrorRange
+    500, // largeErrorTimeout
+    40 // slew rate
+};
+ 
+ 
+// create the chassis
+lemlib::Chassis chassis(drivetrain, lateralController, angularController, sensors);
+
+/**
+ * A callback function for LLEMU's center button.
+ *
+ * When this callback is fired, it will toggle line 2 of the LCD text between
+ * "I was pressed!" and nothing.
+ */
+void on_center_button() {
+	static bool pressed = false;
+	pressed = !pressed;
+	if (pressed) {
+		pros::lcd::set_text(2, "I was pressed!");
+	} else {
+		pros::lcd::clear_line(2);
+	}
+}
+
+/**
+ * Runs initialization code. This occurs as soon as the program is started.
+ *
+ * All other competition modes are blocked by initialize; it is recommended
+ * to keep execution time for this mode under a few seconds.
+ */
+void initialize() {
+	pros::lcd::initialize(); // initialize brain screen
+    chassis.calibrate(); // calibrate sensors
+
+    // the default rate is 50. however, if you need to change the rate, you
+    // can do the following.
+    // lemlib::bufferedStdout().setRate(...);
+    // If you use bluetooth or a wired connection, you will want to have a rate of 10ms
+
+    // for more information on how the formatting for the loggers
+    // works, refer to the fmtlib docs
+
+    // thread to for brain screen and position logging
+    pros::Task screenTask([&]() {
+        lemlib::Pose pose(0, 0, 0);
+        while (true) {
+            // print robot location to the brain screen
+            pros::lcd::print(0, "X: %f", chassis.getPose().x); // x
+            pros::lcd::print(1, "Y: %f", chassis.getPose().y); // y
+            pros::lcd::print(2, "Theta: %f", chassis.getPose().theta); // heading
+            pros::delay(50);
+        }
+    });
+}
+
+/**
+ * Runs while the robot is in the disabled state of Field Management System or
+ * the VEX Competition Switch, following either autonomous or opcontrol. When
+ * the robot is enabled, this task will exit.
+ */
+void disabled() {}
+
+/**
+ * Runs after initialize(), and before autonomous when connected to the Field
+ * Management System or the VEX Competition Switch. This is intended for
+ * competition-specific initialization routines, such as an autonomous selector
+ * on the LCD.
+ *
+ * This task will exit when the robot is enabled and autonomous or opcontrol
+ * starts.
+ */
+void competition_initialize() {}
+
+/**
+ * Runs the user autonomous code. This function will be started in its own task
+ * with the default priority and stack size whenever the robot is enabled via
+ * the Field Management System or the VEX Competition Switch in the autonomous
+ * mode. Alternatively, this function may be called in initialize or opcontrol
+ * for non-competition testing purposes.
+ *
+ * If the robot is disabled or communications is lost, the autonomous task
+ * will be stopped. Re-enabling the robot will restart the task, not re-start it
+ * from where it left off.
+ */
+void autonomous() {}
+
+/**
+ * Runs the operator control code. This function will be started in its own task
+ * with the default priority and stack size whenever the robot is enabled via
+ * the Field Management System or the VEX Competition Switch in the operator
+ * control mode.
+ *
+ * If no competition control is connected, this function will run immediately
+ * following initialize().
+ *
+ * If the robot is disabled or communications is lost, the
+ * operator control task will be stopped. Re-enabling the robot will restart the
+ * task, not resume it from where it left off.
+ */
+
+
+void opcontrol() {
+	pros::Controller master(pros::E_CONTROLLER_MASTER);
+	pros::Motor left_mtr(1);
+	pros::Motor right_mtr(2);
+
+	while (true) {
+        // get joystick positions
+        int leftY = controller.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y);
+        int rightX = controller.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_X);
+        // move the chassis with curvature drive
+        chassis.curvature(leftY, rightX, 2.2);
+        // delay to save resources
+        pros::delay(10);
+	}
+}
