0c38babcb967a4f850dacdbe921866a3b7bd1846
sreyas-p <150875815+sreyas-p@users.noreply.github.com>
2024-05-20 22:19:28 -0700

    add auton winpoint path

--- main.cpp
+++ main.cpp
@@ -142,22 +142,13 @@ void competition_initialize() {}
  * will be stopped. Re-enabling the robot will restart the task, not re-start it
  * from where it left off.
  */
-void autonomous() {}
+ASSET(autonwppath_2024HiS_txt)
 
-/**
- * Runs the operator control code. This function will be started in its own task
- * with the default priority and stack size whenever the robot is enabled via
- * the Field Management System or the VEX Competition Switch in the operator
- * control mode.
- *
- * If no competition control is connected, this function will run immediately
- * following initialize().
- *
- * If the robot is disabled or communications is lost, the
- * operator control task will be stopped. Re-enabling the robot will restart the
- * task, not resume it from where it left off.
- */
-
+void autonomous() {
+    // set chassis pose
+    chassis.setPose(0, 0, 0);
+    chassis.follow(autonwppath_2024HiS_txt, 15, 2000);
+}
 
 void opcontrol() {
 	pros::Controller master(pros::E_CONTROLLER_MASTER);
